a0cebeec9fcaecda2d50fdeea5dfdf13a99ba3ed
giorgiagalasso <giorgiagalasso5@gmail.com>
2021-07-02 11:41:25 +0100

    lets try again

--- models/User.model.js
+++ models/User.model.js
@@ -1,14 +1,19 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
+
 const userSchema = new Schema({
   username: {
     type: String,
-    // unique: true -> Ideally, should be unique, but its up to you
+    required: true,
+    unique: true
   },
-  password: String,
+  password: { 
+    type: String,
+    required: true,
+  },
+  role: String, //Admin, Reader, Guest, Superuser , etc
+  imageUrl: String,
 });
 
 const User = model("User", userSchema);
-
 module.exports = User;
